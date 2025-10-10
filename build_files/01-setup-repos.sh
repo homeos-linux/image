cfb12e829178a141862268618bb02be508c69c92
koyu <me@koyu.space>
2025-10-10 03:19:44 +0200

    Add dnf-plugins-core installation to RPM repository setup script

--- build_files/01-setup-repos.sh
+++ build_files/01-setup-repos.sh
@@ -5,6 +5,9 @@ set -ouex pipefail
 
 echo "=== Setting up RPM repositories ==="
 
+# Add config-manager dnf plugin
+dnf install -y dnf-plugins-core
+
 # Add RPM Fusion repositories
 echo "Adding RPM Fusion repositories..."
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
